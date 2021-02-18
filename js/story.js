b1ffdb190fe16bb73e9ee30f63defd169fef5492
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-02-19 02:28:18 +0900

    add story popup and click red => gray

--- js/story.js
+++ js/story.js
@@ -1,3 +1,7 @@
-function story_color_change() {
-    document.getElementById("story_color").style.border = "2px solid gray";
+function story_color_change(a) {
+    document.getElementById(a).style.border = "2px solid gray"
+}
+
+function story_popup(b){
+    window.open(b,"pop","width=100 height=100 top=300 left=600");
 }
